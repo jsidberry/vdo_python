041dc174660ad4b4fc79460f1d1f165714a8b572
Juan Sidberry <jsidberry@gmail.com>
2021-05-17 17:19:34 -0400

    add db and cdn config

--- utils.py
+++ utils.py
@@ -1,2 +1,24 @@
 import boto3
+import config
+import mysql.connector
 
+options = {
+    'version' : 'latest',
+    'region'  : config['s3']['region'],
+    'key'     : config['s3']['key'],
+    'secret'  : config['s3']['secret']
+}
+
+s3client = boto3.client('s3')
+client   = boto3.client('cloudfront')
+
+
+def database_connection():
+    mydb = mysql.connector.connect(
+        host=config['database']['server'],
+        database=config['database']['name'],
+        user=config['database']['username'],
+        password=config['database']['password']
+    )
+
+    print(mydb) 
