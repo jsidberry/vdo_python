ad943f17b35aa4525a2fe76643167d6e351e0913
Juan Sidberry <jsidberry@gmail.com>
2021-05-17 19:04:06 -0400

    update html templates for login

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return render_template("login.html")
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('welcome'))
+    return render_template('login.html', error=error)
 
-@app.route("/login")
-def vdo_test():
-    return render_template("login.html")
+# landing page after login
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
