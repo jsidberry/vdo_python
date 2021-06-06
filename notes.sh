f6d8434065c3115817767b4a3aaa23a9f9d2c972
Juan Sidberry <jsidberry@gmail.com>
2021-06-06 00:05:59 -0400

    update bootstrap

--- notes.sh
+++ notes.sh
@@ -1,9 +1,12 @@
 # commands and notes to setup and use 
 # Flask Virtual Environemnts for Development
 
-pyenv exec python3 -m venv .venv
+# pyenv exec python3 -m venv .venv
+python3 -m venv .venv
 source .venv/bin/activate
-pip install Flask 
+pip install --upgrade pip
+pip install -r requirements.txt
+# pip install Flask 
 
 export FLASK_APP=app.py
 export FLASK_ENV=development
